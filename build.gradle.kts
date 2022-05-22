145ac22f1de371350f7a9d8e02c5e9c6ee76319c
Jilence <powwwerskill@gmail.com>
2022-05-22 13:29:29 +0200

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,9 @@
+import org.jetbrains.kotlin.scripting.definitions.StandardScriptDefinition.template
+
 val javaVersion = 17 // Minecraft 1.18 requires Java 17
+
+val groupId = "template"
+val main = "TemplateKt"
 
 plugins {
     kotlin("jvm") version "1.6.21"
@@ -15,6 +20,12 @@ repositories {
     mavenLocal()
     maven("https://repo.codemc.io/repository/maven-snapshots/")
     maven("https://repo.papermc.io/repository/maven-public")
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    manifest {
+        attributes["Main-Class"] = "com.jilence.$groupId.$main"
+    }
 }
 
 dependencies {
